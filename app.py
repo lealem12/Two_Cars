357ae71b44010a01d1dce2f33d62267e732a0aa0
lealem <birhanumelesse12@gmail.com>
2024-05-23 21:37:00 +0300

    ground scrolling

--- app.py
+++ app.py
@@ -4,38 +4,50 @@ from pygame.locals import *
 
 
 
+# game variables
+running = True
+ground_scroll = 0
+scroll_speed = 4
+lane_divider = 42 # length of each short, white lane dividers
+
 # screen and functionality
 screen_width = 400
 screen_height = 500
 car_width = 60
 car_height = 135
-# the middle double-line is 20 px
-middle_line = 20
-lane_width = (screen_width - middle_line)//4 
+middle_line = 20 # the yellow double-line in the middle is 20 px
+lane_width = (screen_width - middle_line)//4
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("2 CARS")
 
 
+fps = 80
+clock = pygame.time.Clock()
+
+
 #images
 background_lane = pygame.image.load("img/background_lane.png")
-background_lane = pygame.transform.scale(background_lane, (screen_width, screen_height))
+background_lane = pygame.transform.scale(background_lane, (screen_width, screen_height + lane_divider))
+
 
 blue_car = pygame.image.load("img/blue_car.png")
 blue_car = pygame.transform.scale(blue_car, (car_width, car_height))
 green_car = pygame.image.load("img/green_car.png")
 green_car = pygame.transform.scale(green_car, (car_width, car_height))
 
-# game variables
-running = True
-
-
 
 #main game loop
 
 while running:
+    clock.tick(fps)
     
     #drawing backrounds and cars
-    screen.blit(background_lane, (0, 0))
+    screen.blit(background_lane, (0, ground_scroll - lane_divider))
+    ground_scroll += scroll_speed
+    if ground_scroll > lane_divider:
+        ground_scroll = 0
+
+
     screen.blit(blue_car, (lane_width + (lane_width - car_width)/2, screen_height - 1.1 * car_height))
     screen.blit(green_car, (2 * lane_width + middle_line + (lane_width - car_width)/2, screen_height - 1.1 * car_height))
     
@@ -52,16 +64,6 @@ while running:
 
 
 pygame.quit()
-
-
-
-
-
-
-
-
-
-
 
 
 
